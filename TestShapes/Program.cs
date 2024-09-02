3a351f7f2e27b9a0f146e631b3967c3a795e903b
KomaMix <ksabirov448@gmail.com>
2024-09-02 20:49:56 +0300

    Добавил примитивную логику проверки функциональности в Program.cs

--- TestShapes/Program.cs
+++ TestShapes/Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿using TestShapes.Shapes;
+
+IShape shape;
+
+Random rand = new Random();
+
+bool flag = rand.Next(0, 2) == 1;
+
+if (flag)
+{
+    shape = new Triangle(3, 4, 5);
+} else
+{
+    shape = new Circle(2);
+}
+
+Console.WriteLine("Тип фигуры: " + shape.GetType().Name);
+Console.WriteLine("Площадь фигуры: " + shape.GetArea());
+
+if (shape.GetType() == typeof(Triangle) && shape is not null)
+{
+    Console.WriteLine("Этот треугольник " +
+        ((shape as Triangle).IsStupidTriangle() ? "тупой" : "обычный"));
+}
